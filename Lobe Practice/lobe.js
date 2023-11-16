8cbc44f62a6208663653388b6da7079720850fbb
jhexterr <143709592+jhexterr@users.noreply.github.com>
2023-11-16 21:55:37 +0800

    Add files via upload

---
+++ Lobe Practice/lobe.js
@@ -0,0 +1,7 @@
+const links = document.querySelectorAll(".footer-link h4");
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    link.nextElementSibling.classList.toggle("open");
+    link.querySelector("i").classList.toggle("open");
+  });
+});
