fe9b8ae61137953529931dc8010dd32190614b91
jhexterr <143709592+jhexterr@users.noreply.github.com>
2023-11-22 07:52:39 +0800

    Delete Lobe Practice directory

--- Lobe Practice/lobe.js
+++
@@ -1,7 +0,0 @@
-const links = document.querySelectorAll(".footer-link h4");
-links.forEach((link) => {
-  link.addEventListener("click", () => {
-    link.nextElementSibling.classList.toggle("open");
-    link.querySelector("i").classList.toggle("open");
-  });
-});
