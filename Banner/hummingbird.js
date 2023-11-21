1ad919ff815dcc905f52cb503864f3e7dbb75fd0
jhexterr <143709592+jhexterr@users.noreply.github.com>
2023-11-22 07:52:52 +0800

    Delete Banner directory

--- Banner/hummingbird.js
+++
@@ -1,6 +0,0 @@
-const bar = document.querySelector(".burger-btn");
-const sp = document.querySelector("span");
-bar.addEventListener("click", () => {
-    bar.classList.toggle("open");
-    sp.classList.toggle("open");
-})
