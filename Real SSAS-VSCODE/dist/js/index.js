88c0f6daa6e7c520540dae4d86d67602b8a437c6
jhexterr <143709592+jhexterr@users.noreply.github.com>
2023-11-22 08:12:35 +0800

    Delete Real SSAS-VSCODE directory

--- Real SSAS-VSCODE/dist/js/index.js
+++
@@ -1,6 +0,0 @@
-const headerNav = document.querySelector(".header-nav");
-const burgerBtn = document.querySelector(".first-burger");
-burgerBtn.addEventListener("click", () => {
-    headerNav.classList.toggle("open");
-    burgerBtn.classList.toggle("open");
-});
